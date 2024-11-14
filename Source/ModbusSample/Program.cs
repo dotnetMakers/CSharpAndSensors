bc795120c53f4eaa5de9e5068d52304543697dbc
Chris Tacke <ctacke@opennetcf.com>
2024-11-14 11:57:25 -0600

    modbus tcp client sample

---
+++ Source/ModbusSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using Meadow.Modbus;
+
+namespace ModbusSample
+{
+    internal class Program
+    {
+        private static bool _doInputTest;
+
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Modbus");
+
+            var client = new ModbusTcpClient("192.168.0.9");
+            await client.Connect();
+
+            while (true)
+            {
+                if (_doInputTest)
+                {
+                    var value = await client.ReadHoldingRegisters(1, 0, 1);
+
+                    Console.WriteLine(value[0]);
+
+                    await Task.Delay(1000);
+                }
+                else
+                {
+                    Console.Write("Enter a rate: ");
+                    var val = Console.ReadLine();
+                    if (ushort.TryParse(val, out ushort rate))
+                    {
+                        await client.WriteHoldingRegister(1, 0, rate);
+                    }
+                }
+            }
+        }
+    }
+}
