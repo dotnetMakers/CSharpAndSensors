8b884c824f1cf63c83c2ce66e694f3a0455eb6bf
Chris Tacke <ctacke@opennetcf.com>
2024-12-23 09:25:29 -0600

    ENS160 Sample

---
+++ Source/ENS160Sample/MyApplication.cs
@@ -0,0 +1,38 @@
+﻿using Meadow;
+using Meadow.Foundation.Sensors.Environmental;
+using YoshiPi;
+
+namespace ENS160Sample
+{
+    internal sealed class MyApplication : YoshiPiApp
+    {
+        private Ens160 _sensor;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize");
+
+            _sensor = new Ens160(Hardware.Qwiic, Ens160.Addresses.Address_0x53);
+            _sensor.EthanolConcentrationUpdated += OnEthanolConcentrationUpdated;
+            _sensor.StartUpdating();
+
+            Resolver.Services.Add(
+                new DisplayService(Hardware.Display, Hardware.Touchscreen)
+                );
+
+            return base.Initialize();
+        }
+
+        private void OnEthanolConcentrationUpdated(object? sender, IChangeResult<Meadow.Units.Concentration> e)
+        {
+            var display = Resolver.Services.Get<DisplayService>();
+
+            display?.SetLabelText($"{e.New.PartsPerThousand} ppt");
+        }
+
+        public static async Task Main(string[] args)
+        {
+            await MeadowOS.Start(args);
+        }
+    }
+}
