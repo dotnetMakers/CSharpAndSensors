c52553c213c91639ee4ee1b702d1caa96475cca7
Chris Tacke <ctacke@opennetcf.com>
2025-01-27 18:05:17 -0600

    ultrasonic sensor

---
+++ Source/UltrasonicSample/MyApplication.cs
@@ -0,0 +1,48 @@
+﻿using Meadow;
+using Meadow.Foundation.Sensors.Distance;
+using YoshiPi;
+
+namespace UltrasonicSample;
+
+internal sealed class MyApplication : YoshiPiApp
+{
+    private A02yyuw _sensor;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize");
+
+        Resolver.Services.Add(
+            new DisplayService(Hardware.Display, Hardware.Touchscreen)
+            );
+
+        _sensor = new A02yyuw(Hardware.MikroBus.CreateSerialPort(9600));
+        _sensor.Updated += OnSensorUpdated;
+        _sensor.StartUpdating(TimeSpan.FromMilliseconds(500));
+
+        return base.Initialize();
+    }
+
+    private void OnSensorUpdated(object? sender, IChangeResult<Meadow.Units.Length> e)
+    {
+        var display = Resolver.Services.Get<DisplayService>();
+
+        display.SetLabelText($"Distance: {e.New.Centimeters:N1}");
+    }
+
+    public override Task Run()
+    {
+        Resolver.Log.Info("Run");
+
+        var display = Resolver.Services.Get<DisplayService>();
+
+        display?.SetLabelText("Hello YoshiPi!");
+
+        return base.Run();
+    }
+
+    public static async Task Main(string[] args)
+    {
+        await MeadowOS.Start(args);
+    }
+}
