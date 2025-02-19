01dd19114d670b9ad660f35f258669c764f464eb
Chris Tacke <ctacke@opennetcf.com>
2025-02-19 10:02:20 -0600

    color sample

---
+++ Source/ColorSample/MyApplication.cs
@@ -0,0 +1,37 @@
+﻿using Meadow;
+using Meadow.Foundation.Sensors.Color;
+using YoshiPi;
+
+namespace ColorSample;
+
+internal sealed class MyApplication : YoshiPiApp
+{
+    private Tcs3472x _sensor;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize");
+
+        _sensor = new Tcs3472x(Hardware.MikroBus.I2cBus);
+        _sensor.Updated += OnSensorUpdated;
+        _sensor.StartUpdating(TimeSpan.FromSeconds(1));
+
+        Resolver.Services.Add(
+            new DisplayService(Hardware.Display, Hardware.Touchscreen)
+            );
+
+        return base.Initialize();
+    }
+
+    private void OnSensorUpdated(object? sender, IChangeResult<Color> e)
+    {
+        var display = Resolver.Services.Get<DisplayService>();
+        display.SetLabelText($"RGB: [{e.New.R},{e.New.G},{e.New.B}]");
+        display.SetBackColor(e.New);
+    }
+
+    public static async Task Main(string[] args)
+    {
+        await MeadowOS.Start(args);
+    }
+}
