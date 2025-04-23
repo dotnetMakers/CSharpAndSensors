c920fd7b495d78c8cf5c99119c55e3c712c7452e
Chris Tacke <ctacke@opennetcf.com>
2025-04-22 21:17:06 -0500

    battery monitor sample

---
+++ Source/BatteryMonitorSample/MeadowApp.cs
@@ -0,0 +1,24 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System.Threading.Tasks;
+
+namespace BatteryMonitorSample
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+
+            return base.Initialize();
+        }
+
+        public override Task Run()
+        {
+            Resolver.Log.Info("Run...");
+
+            return base.Run();
+        }
+    }
+}
